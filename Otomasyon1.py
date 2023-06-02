22d9a49198d5dccb7aaf24468fa6466a9ed345b1
Gizem Yılmaz <64495695+00Gizem00@users.noreply.github.com>
2023-06-02 13:56:19 +0300

    ver1 olusturuldu

--- Otomasyon1.py
+++ Otomasyon1.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+import time
 
 conn = sqlite3.connect('Student.db')
 curser = conn.cursor()
@@ -24,3 +25,47 @@ def list_student():
         print(student[0],"\t\t",student[1],"\t\t",student[2],"\t\t",student[3],"\t\t",student[4])
     print("------------------------------------------------------------------------------------------------------------------")
     menu()
+
+def add_student():
+    os.system('cls')
+    input_name = input("Enter name: ")
+    input_surname = input("Enter surname: ")
+    input_age = input("Enter age: ")
+    input_gender = input("Enter gender: ")
+    
+    curser.execute("INSERT INTO student (name, surname, age, gender) VALUES (?, ?, ?, ?)", (input_name, input_surname, input_age, input_gender))
+    conn.commit()
+    conn.close()
+
+    print("Student added successfully.")
+    menu()
+
+def delete_student():
+    os.system('cls')
+    input_id = input("Enter id: ")
+    curser.execute("DELETE FROM student WHERE st_id = ?", (input_id,))
+    conn.commit()
+    conn.close()
+
+    print("Student deleted successfully.")
+    menu()
+
+
+while option != 0:
+    if option == 1:
+        list_student()
+    elif option == 2:
+        add_student()
+    elif option == 3:
+        delete_student()
+    else:
+        print("Invalid option.")
+        time.sleep(1)
+        os.system('cls')
+        menu()
+
+
+print("Exiting the program...")
+time.sleep(1)
+os.system('cls')
+print("Program exited.")
